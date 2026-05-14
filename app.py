83f774bb566a139bac1a841609a0b39606b4d944
mogadisu56-hub <mogadisu.56@gmail.com>
2026-05-14 09:24:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -317,7 +317,7 @@ with ana_sekme2:
             # A- SARCODINA
             st.markdown("<p style='font-weight:bold; color:#8b0000; margin-bottom:5px;'>A- SARCODINA</p>", unsafe_allow_html=True)
             with st.expander("Amipler"):
-                amipler = ["Entamoeba histolytica", "Entamoeba dispari", "Entamoeba polecki", "Entamoeba coli", 
+                amipler = ["Entamoeba histolytica", "Entamoeba dispar", "Entamoeba polecki", "Entamoeba coli", 
                            "Endolimax nana", "Iodamoeba bütschlii", "Entamoeba hartmanni", "Entamoeba gingivalis"]
                 for p in amipler: parazit_yazdir(p)
             
